bdc02971aea6d87ae8fe833454a425460d97f0eb
dexos231 <otmaneotmanoviche@gmail.com>
2023-08-07 03:59:26 -0700

    update

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -29,9 +29,10 @@ int main(int argc, char *argv[])
 	if(num < 0)
 	{
 		printf("0\n");
+		return(0);
 	}
 
-	for(j = 0; j < 5 && num >= 0; j++ ) 
+	for(j = 0; j < 5 && num >= 0; j++) 
 	{
 		while(num >= coins[j])
 		{
